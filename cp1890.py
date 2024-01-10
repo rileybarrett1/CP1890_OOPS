6ae822e2fc515e55e395c64e54bd2bebfdbe6960
rileybarrett1 <rileybarrett7@gmail.com>
2024-01-10 09:50:44 -0330

    numbers

---
+++ cp1890.py
@@ -0,0 +1,15 @@
+fp_number =12345.6789
+fp_number2 = 12345612345.6789
+fp_number3=.6789
+dc_number= 6789
+print(f"{fp_number:.2f} ")
+
+print(f"{fp_number:,.2f}")
+
+print(f"{fp_number2:15,.2f}")
+
+##different types of codes
+print(f"{fp_number3:.0%}")
+print(f"{fp_number3:.1%}")
+
+print(f"{dc_number:d}")
